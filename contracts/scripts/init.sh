283d1867438ac5fcde71970f2709b5c06f9729f3
i3ima <i3ima@mephedr.one>
2022-02-01 01:57:46 +0300

    refactor(scripts): change init script accordingly to changes in factory

--- contracts/scripts/init.sh
+++ contracts/scripts/init.sh
@@ -1,11 +1,18 @@
 # Add credentials
 source neardev/dev-account.env
 
+echo "Enter your near account name (example: test.testnet)"
+read -r ID
+
 # Init factory
 near call "$CONTRACT_NAME" new --accountId "$CONTRACT_NAME";
 
 # Create FT subaccount
-near call "$CONTRACT_NAME" create_ft '{ "prefix": "test", "reference": "some_json" }' --accountId "$CONTRACT_NAME" --deposit 3;
+near call "$CONTRACT_NAME" create_ft '{ "account_id": "'$ID'", "reference": "some_json" }' --accountId "$CONTRACT_NAME" --deposit 3;
 
 # Test some stuff
-near call "$CONTRACT_NAME" give_to '{ "buyer": "tester", "seller": "test", "amount": 1 }' --accountId "$CONTRACT_NAME";
+
+# Get prefix cuz yes
+#ID_PREFIX="$(cut -d'.' -f1 <<<"$ID")"
+
+near call "$CONTRACT_NAME" give_to '{ "buyer": "tester.testnet", "seller": "'$ID'", "amount_near": 1, "amount_ft": 2 }' --accountId "$CONTRACT_NAME"
