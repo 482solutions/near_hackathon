a55799ea91dda38d2e4e9e159a968f6fd54580ff
Dmitrii-Kholodov <dmytro.kholodov@482.solutions>
2022-01-31 14:47:45 +0200

    update prettier/linter configs

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
