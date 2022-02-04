fe0a09c238fb46dc36a0b087ff5118e459571c74
i3ima <i3ima@mephedr.one>
2022-02-04 20:11:28 +0300

    feat(ft): create wrapper for calling from market

---
+++ contracts/ft/src/internal.rs
@@ -0,0 +1,8 @@
+use crate::*;
+
+impl Contract {
+    pub fn register_resolve(&mut self, account_id: AccountId) {
+        log!("User {} is not registered yet. Doing it now", &account_id);
+        self.token.internal_register_account(&account_id);
+    }
+}
