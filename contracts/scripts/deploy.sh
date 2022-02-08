2945e66e40157988d69b77f183ddb1766dfbb36a
i3ima <i3ima@mephedr.one>
2022-02-08 21:36:59 +0300

    feat(scripts): introducing ready for production scripts

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -5,20 +5,14 @@ else
   echo "Using previous dev-account"
 fi
 
-if [ -d neardev ]; then
-    echo "Already logged in"
-else
-    near login
-fi
-
 echo "Building contracts"
 sh scripts/build.sh
 
 if [[ -z "${DEV}" ]]; then
     echo "Deploying to testnet"
     
-    echo "Deploying market"
-    near dev-deploy out/market.wasm
+    echo "Deploying Factory"
+    near dev-deploy out/token_factory.wasm
 else
     echo "Standard deploy"
     near deploy
--- contracts/scripts/init.sh
+++ contracts/scripts/init.sh
@@ -1,5 +1,14 @@
 # Add credentials
 source neardev/dev-account.env
 
-# Create FT subaccount
+# Create Market subaccount
+near create-account market."$CONTRACT_NAME" --masterAccount "$CONTRACT_NAME"
+
+# Deploy contract to it
+near deploy market."$CONTRACT_NAME" out/market.wasm
+
+# Init FT factory
 near call "$CONTRACT_NAME" new --accountId "$CONTRACT_NAME"
+
+# Init market
+near call market."$CONTRACT_NAME" new '{ "owner_id": "'"$CONTRACT_NAME"'"}' --accountId "$CONTRACT_NAME"
