e8e883d5470e35d188e22b819ff413e320afbe63
i3ima <i3ima@mephedr.one>
2022-02-16 08:34:22 +0300

    feat(backend): move keys loading into separate class

---
+++ server/src/types/session.ts
@@ -0,0 +1,38 @@
+import { ConnectConfig, KeyPair, keyStores } from "near-api-js";
+import { UnencryptedFileSystemKeyStore } from "near-api-js/lib/key_stores";
+import * as os from "os";
+import * as path from "path";
+
+const homedir = os.homedir();
+
+const CREDENTIALS_DIR = ".near-credentials";
+const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+
+export class Session {
+  public readonly id: string;
+  private readonly store: UnencryptedFileSystemKeyStore;
+
+  defaultConfig(): ConnectConfig {
+    return {
+      headers: {},
+      networkId: "testnet",
+      keyStore: this.store,
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org"
+    }
+  }
+
+  async addToStore(id: string, pair: KeyPair) {
+    await this.store.setKey("testnet", id, pair)
+  }
+
+  getStore() {
+    return this.store
+  }
+
+  constructor(id: string) {
+    this.id = id;
+    this.store = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+  }
+}
