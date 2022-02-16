27a98dfcbffec5ad1d3b2236a90f081db00aa487
i3ima <i3ima@mephedr.one>
2022-02-16 12:08:30 +0300

    chore: use pk

--- server/src/near/near.service.ts
+++ server/src/near/near.service.ts
@@ -3,8 +3,12 @@ import { Session } from "../types/session";
 import { ConfigService } from "@nestjs/config";
 import { Account, connect, KeyPair, Near } from "near-api-js";
 import { BN } from "bn.js";
+import { FinalExecutionOutcome } from "near-api-js/lib/providers";
 
+export const NO_DEPOSIT = new BN("0", 10);
+export const DEFAULT_GAS = new BN("300000000000000", 10);
 const INITIAL_BALANCE = new BN("3464010000000000000000000", 10);
+
 
 @Injectable()
 export class NearService {
@@ -15,7 +19,7 @@ export class NearService {
 
   constructor(private configService: ConfigService) {
     this.logger.log("Creating session");
-    this.session = new Session(configService.get("ID"));
+    this.session = new Session(configService.get("ID"), configService.get("PRIVATE_KEY"));
     this.init();
   }
 
@@ -38,6 +42,16 @@ export class NearService {
     return await this.ownerAccount.createAndDeployContract(account, keyPair.getPublicKey(), contract, INITIAL_BALANCE)
   }
 
+  async initContract(account: Account, method: string, args: object): Promise<FinalExecutionOutcome> {
+      return await account.functionCall({
+        contractId: account.accountId,
+        methodName: method,
+        args,
+        gas: DEFAULT_GAS,
+        attachedDeposit: NO_DEPOSIT
+      })
+  }
+
   // TODO: Add saving of keyPair to JSON-file
   async createSubAccount(id: string): Promise<Account> {
     const account = `${id}.${this.session.id}`;
@@ -46,7 +60,7 @@ export class NearService {
 
     await this.session.addToStore(id, keyPair);
     await this.ownerAccount.createAccount(account, publicKey, INITIAL_BALANCE);
-    await this.ownerAccount.addKey(publicKey)
+    await this.ownerAccount.addKey(publicKey, id)
 
     return await this.subAccount(id);
   }
