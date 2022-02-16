b732e1ae392da49e9fc955d84d813ed6f5c6c944
i3ima <i3ima@mephedr.one>
2022-02-16 14:56:40 +0300

    feat(backend): save credentials after account creation

--- server/src/types/session.ts
+++ server/src/types/session.ts
@@ -2,11 +2,19 @@ import { ConnectConfig, KeyPair, keyStores } from "near-api-js";
 import { UnencryptedFileSystemKeyStore } from "near-api-js/lib/key_stores";
 import * as os from "os";
 import * as path from "path";
+import * as fs from "fs";
+import { KeyPairEd25519 } from "near-api-js/lib/utils";
 
 const homedir = os.homedir();
 
 const CREDENTIALS_DIR = ".near-credentials";
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+
+interface Credentials {
+  account_id: string,
+  public_key: string,
+  private_key: string
+}
 
 export class Session {
   public readonly id: string;
@@ -23,8 +31,14 @@ export class Session {
     };
   }
 
-  async addToStore(id: string, pair: KeyPair) {
+  async addToStore(id: string, pair: KeyPairEd25519) {
     await this.store.setKey("testnet", id, pair);
+    const creds: Credentials = {
+      account_id: id,
+      public_key: pair.publicKey.toString(),
+      private_key: pair.secretKey
+    }
+    fs.writeFileSync(`${credentialsPath}/testnet/${id}.json`, JSON.stringify(creds))
   }
 
   getStore() {
