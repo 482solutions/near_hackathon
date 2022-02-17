cc1a474098c2b2ccd7d297c42924b86f6343f2b4
i3ima <i3ima@mephedr.one>
2022-02-17 19:38:09 +0300

    feat(backend): TASK-BASED MARKET UPDATE YAY

---
+++ server/src/tasks/tasks.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
+import { MarketService } from "../market/market.service";
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+  private readonly market: MarketService;
+
+  constructor(market: MarketService) {
+    this.market = market;
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async updateMarket() {
+    this.logger.log("Executing market update task");
+    await this.market.update_market()
+  }
+}
