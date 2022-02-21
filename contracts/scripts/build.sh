16627352f1f01acd9c6eaa60b7f3f77bb55c9850
i3ima <i3ima@mephedr.one>
2022-02-21 16:14:34 +0300

    chore: add erc-1155 impl to build script

---
+++ contracts/scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+OUT_DIR=out
+
+if [ ! -d $OUT_DIR ]; then
+    echo "Creating '${OUT_DIR}' directory"
+    mkdir $OUT_DIR;
+fi
+
+# Because we include bytes from FT contract we need to build it before factory
+
+cargo build --release -p nft
+cp target/wasm32-unknown-unknown/release/nft.wasm $OUT_DIR
+
+cargo build --release -p market
+cp target/wasm32-unknown-unknown/release/market.wasm $OUT_DIR
+
+cargo build --release -p near-1155
+cp target/wasm32-unknown-unknown/release/near_1155.wasm $OUT_DIR
