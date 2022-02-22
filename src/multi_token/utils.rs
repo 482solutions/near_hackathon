a21b2d1533e6f8b8a30ea3e9be970b070b1e78d0
i3ima <i3ima@mephedr.one>
2022-02-22 16:54:09 +0300

    feat(utils): refund, storage calculation, security assert

--- src/multi_token/utils.rs
+++ src/multi_token/utils.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::Display,
+    mem::size_of,
+};
+
 use near_sdk::{env, require, AccountId, Balance, CryptoHash, Promise};
 
 pub fn hash_account_id(account_id: &AccountId) -> CryptoHash {
@@ -19,4 +24,38 @@ pub fn refund_deposit_to_account(storage_used: u64, account_id: AccountId) {
     if refund > 1 {
         Promise::new(account_id).transfer(refund);
     }
+}
+
+/// Assumes that the precedecessor will be refunded
+pub fn refund_deposit(storage_used: u64) {
+    refund_deposit_to_account(storage_used, env::predecessor_account_id())
+}
+
+// TODO: need a way for end users to determine how much an approval will cost.
+pub fn bytes_for_approved_account_id(account_id: &AccountId) -> u64 {
+    // The extra 4 bytes are coming from Borsh serialization to store the length of the string.
+    account_id.as_str().len() as u64 + 4 + size_of::<u64>() as u64
+}
+
+pub enum Entity {
+    Contract,
+    Token,
+}
+
+impl Display for Entity {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let as_string = match self {
+            Entity::Contract => "contract",
+            Entity::Token => "token",
+        };
+        write!(f, "{}", as_string)
+    }
+}
+
+pub fn expect_approval<T>(o: Option<T>, entity: Entity) -> T {
+    o.expect(format!("Approval Management is not supported by {}", entity.to_string()).as_str())
+}
+
+pub fn unauthorized_assert(account_id: &AccountId) {
+    require!(account_id == &env::predecessor_account_id())
 }
